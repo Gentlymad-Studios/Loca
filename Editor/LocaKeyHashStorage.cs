850623dd14daf7d487d3241f37da6d454b726d41
Markus <marco.polo.1994@web.de>
2023-06-06 12:19:04 +0200

    method is now static...

--- Editor/LocaKeyHashStorage.cs
+++ Editor/LocaKeyHashStorage.cs
@@ -91,7 +91,7 @@ namespace Loca {
             return displayArray[index];
         }
 
-        public int GetHashFromString(string value) {
+        public static int GetHashFromString(string value) {
             for (int i = 0; i < displayArray.Length; i++) {
                 if (value == displayArray[i]) {
                     return GetHashFromIndex(i);
